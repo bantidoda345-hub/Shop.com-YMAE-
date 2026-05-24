03f7c11d3ca94a042f59c0c6d2b138eb00964104
bantidoda345-hub <bantidoda345@gmail.com>
2026-05-24 15:47:25 +0530

    Add password verification handler in Verify.js

---
+++ api/Verify.js
@@ -0,0 +1,27 @@
+export default function handler(req, res) {
+
+const PASSWORD = process.env.WEB_PASSWORD;
+
+if(req.method !== 'POST'){
+return res.status(405).json({
+success:false
+});
+}
+
+const { password } = req.body;
+
+if(password === PASSWORD){
+
+return res.status(200).json({
+success:true
+});
+
+}else{
+
+return res.status(401).json({
+success:false
+});
+
+}
+
+}
